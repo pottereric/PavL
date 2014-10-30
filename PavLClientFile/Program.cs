0e237e5b7a2c0b0581c1f60f997317a9d66149de
pottereric <eric.b.potter@gmail.com>
2014-10-30 10:02:03 -0400

    Added the file logging client

---
+++ PavLClientFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using PavL.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PavLClientFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\MyTest.log";
+
+            if (!File.Exists(path))
+            {
+                // Create a file to write to. 
+                string createText = "Log File" + Environment.NewLine;
+                File.WriteAllText(path, createText);
+            }
+
+
+            var logHandler = new LogHandler();
+
+            logHandler.Listen(s => File.AppendAllText(path, String.Format("{0}{1}", s, Environment.NewLine)));
+
+            Console.WriteLine("Press Any Key to Continue");
+            Console.ReadKey();
+        }
+    }
+}
